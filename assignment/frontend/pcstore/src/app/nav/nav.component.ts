57516e0247e3e78a0d5c417bb08e464a00b61837
Nicky91294 <nicky91294@hotmail.com>
2023-07-03 05:51:14 +0200

    css

--- assignment/frontend/pcstore/src/app/nav/nav.component.ts
+++ assignment/frontend/pcstore/src/app/nav/nav.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { Product } from '../model/product';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
+export class NavComponent implements OnInit{
+  products!: Product[];
+
+  searchFormGroup = new FormGroup({
+    name: new FormControl <string> ('')
+  });
+ 
+
+  
+  constructor(
+    private productService: ProductService,
+    ){}
+  
+    ngOnInit(): void {
+      this.getData();
+    }
+  // filteredOptions: Observable<string[]>;
+  // ngOnInit(): void {
+  //   this.getData();
+  // }
+
+  getData(){
+    this.productService
+      .getAll()
+      .subscribe((data)=> (this.products = data))
+  }
+
+  // onSearch(){
+  //   var temp = this.searchFormGroup.value as Product;
+  // this.productService.getProductbyName(temp.name).subscribe();
+  // }
+
+  
 
 }
